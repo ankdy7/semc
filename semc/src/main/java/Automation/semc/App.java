fa77428cb498aa8c141e51b2fcb212401efb68a3
ankdy7 <ankur.dey.008@gmail.com>
2022-07-12 19:09:43 +0530

    commit

---
+++ semc/src/main/java/Automation/semc/App.java
@@ -0,0 +1,13 @@
+package Automation.semc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
